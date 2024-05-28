75ecc84f98942017cdd8b781acf5490585f00075
08-zimmad <zimmad.w@gmail.com>
2024-05-28 18:18:32 +0500

    added custom Organizer Admin

--- event_nest/authenticate_/admin.py
+++ event_nest/authenticate_/admin.py
@@ -1,6 +1,57 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django import forms
 from .models import Organizer
 
+class OrganizerCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
 
-admin.site.register(Organizer)
-# Register your models here.
+    class Meta:
+        model = Organizer
+        fields = ('email',)
+
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+class OrganizerChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = Organizer
+        fields = ('name','email', 'password', 'is_active', 'is_staff')
+
+class OrganizerAdmin(BaseUserAdmin):
+    form = OrganizerChangeForm
+    add_form = OrganizerCreationForm
+
+    list_display = ('name','email', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('name','email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+admin.site.register(Organizer, OrganizerAdmin)
